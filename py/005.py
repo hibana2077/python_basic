ccff1b9d52f605c2991e4ac2a24eb4be2bfd2c72
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2021-10-29 16:33:32 +0800

    Add files via upload

---
+++ py/005.py
@@ -0,0 +1,9 @@
+a=25
+b="25"
+c=[25]
+print(a)#25
+print(b)#25
+print(c)#[25]
+print("a的type()",type(a))#a的type() <class 'int'>
+print("b的type()",type(b))#b的type() <class 'str'>
+print("c的type()",type(c))#c的type() <class 'list'>
